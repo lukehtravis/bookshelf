8d86c0904705e6a4435150a8f8eb2e5a9cdbe071
luke <lukehtravis@gmail.com>
2017-11-01 14:52:03 -0700

    Test

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,18 +4,19 @@ import React, { Component } from 'react';
 class Shelf extends Component {
 
   render() {
-    console.log(this.props.shelves);
+    console.log(this.props);
     return (
-
-      <div className="bookshelf">
-      <h2 className="bookshelf-title"></h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-            </li>
-          </ol>
+      {this.props.map((shelvie) => (
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelvie}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <li>
+              </li>
+            </ol>
+          </div>
         </div>
-      </div>
+      ))}
     )
   }
 }
