c97a59bb408a91e3921c88a571e401acc3d775bc
luke <lukehtravis@gmail.com>
2017-11-02 14:55:06 -0700

    fix: blank books state
    took out code that was breaking app. blank books state array

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    books: [
-      {fetchName: }
-    ]
   },
   shelves = [
     {fetchName: 'currentlyReading', outputName: 'Currently Reading'},
