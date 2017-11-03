627a548b710030b8134c81ea569cdf788eb7f160
luke <lukehtravis@gmail.com>
2017-11-03 11:25:22 -0700

    feat: add AssignBookToShelf compnent
    code changes in book.js to accomodate as well

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
+import AssignBookToShelf from './AssignBookToShelf.js'
 
 class Books extends Component {
   render() {
@@ -14,15 +15,7 @@ class Books extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            <AssignBookToShelf />
           </div>
           <div className="book-title">{title}</div>
           <div className="book-authors">{author}</div>
