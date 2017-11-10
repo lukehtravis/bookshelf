586368149e9d224f1da2e4def69cc87932e01455
luke <lukehtravis@gmail.com>
2017-11-10 13:45:39 -0800

    feature: implement button to switch between search and list screen

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ class BooksApp extends React.Component {
   };
 
   componentDidMount() {
-  BooksAPI.getAll().then((books) => {
-    this.setState({books})
-  })};
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  };
 
   moveBook = (book,value) => {
-     BooksAPI.update(book,value).then(() => {
-       book.shelf = value
-       this.setState(state => ({
-          books: state.books.filter(b => b.id !== book.id).concat(book) })
-       )
-     })
+    BooksAPI.update(book,value).then(() => {
+      book.shelf = value
+      this.setState(state => ({
+        books: state.books.filter(b => b.id !== book.id).concat(book) })
+      )
+    })
    }
 
   render() {
@@ -49,6 +50,28 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
+      {this.state.showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <div className="search-books-input-wrapper">
+              {/*
+                NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                You can find these search terms here:
+                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                you don't find a specific author or title. Every search is limited by search terms.
+              */}
+              <input type="text" placeholder="Search by title or author"/>
+
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
         <div className="list-books">
           <div className="list-books-title">
             <h1>MyReads</h1>
@@ -91,7 +114,11 @@ class BooksApp extends React.Component {
               </div>
             </div>
           </div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
         </div>
+        )}
       </div>
     )
   }
