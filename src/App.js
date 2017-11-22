9102fc8ecca1da306eb9899f001adf83aedeee87
luke <lukehtravis@gmail.com>
2017-11-21 19:17:54 -0800

    refactor: code refactor to accomodate new shelf compnent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI'
-import Book from './Book.js'
+import Shelf from './Shelf.js'
 import SearchForBook from './SearchForBook.js'
 import './App.css'
 import { Link, Route } from 'react-router-dom'
@@ -8,6 +8,7 @@ import { Link, Route } from 'react-router-dom'
 class BooksApp extends React.Component {
   state = {
     books: [],
+    currentShelf: ['wantToRead', 'currentlyReading', 'read']
   }
 
   componentDidMount() {
@@ -27,22 +28,6 @@ class BooksApp extends React.Component {
 
   render() {
 
-    let want = []
-    let current = []
-    let read = []
-
-    for (let book of this.state.books) {
-      if (book.shelf === "wantToRead") {
-        want.push(book)
-      }
-      if (book.shelf === "currentlyReading") {
-        current.push(book)
-      }
-      if (book.shelf === "read") {
-        read.push(book)
-      }
-    }
-
     return (
       <div className="app">
         <Route path="/search" exact render={() => (
@@ -54,42 +39,9 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <div className="wantToRead">
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want To Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {want.map((book) => {
-                        return <Book key={book.id} onMoveBook={this.moveBook} individualBook={book} />
-                      })}
-                    </ol>
-                  </div>
-                </div>
-              </div>
-              <div className="currentlyReading">
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {current.map((book) => {
-                        return <Book key={book.id} onMoveBook={this.moveBook} individualBook={book} />
-                      })}
-                    </ol>
-                  </div>
-                </div>
-              </div>
-              <div className="read">
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {read.map((book) => {
-                        return <Book key={book.id} onMoveBook={this.moveBook} individualBook={book} />
-                      })}
-                    </ol>
-                  </div>
-                </div>
-              </div>
+              {this.state.currentShelf.map((shelf) => {
+                return <Shelf key={shelf} shelfName={shelf} onMoveBook={this.moveBook} books={this.state.books} />
+              })}
             </div>
             <div className="open-search">
               <Link to="/search">Add a book</Link>
--- src/Shelf.js
+++ src/Shelf.js
@@ -6,14 +6,16 @@ class Shelf extends Component {
 
   render() {
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelvesProp.outputName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.map((book) => {
-              return <Book assignFunc={this.props.assignFunc} individualBook={book} />
-            })}
-          </ol>
+      <div className={this.props.shelfName}>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books.map((book) => {
+                return book.shelf ===  this.props.shelfName ? <Book key={book.id} onMoveBook={this.props.onMoveBook} individualBook={book} /> :  null
+              })}
+            </ol>
+          </div>
         </div>
       </div>
     )
